78c015eb6b1e16c9102106b53252a838bf434a9d
Vaishnavi160904 <133471646+Vaishnavi160904@users.noreply.github.com>
2026-03-10 20:42:24 +0530

    Restructured folder into HandsOn/OOP

--- Practice/try.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int m=10;
-    int n=(10,20,60);
-    cout<<n;
-
-}
--- fundamentals/Indentifiers.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-int main(){
-    int num=-5;
-    std::cout<<sizeof(num);
-    return -1;
-}
